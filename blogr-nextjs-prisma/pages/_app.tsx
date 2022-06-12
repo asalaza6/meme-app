eea1c71597851a8d58b9a6b2bda93be52cf65162
Alex Salazar <55558620+asalaza6@users.noreply.github.com>
2022-06-12 15:09:10 -0700

    add toast

--- blogr-nextjs-prisma/pages/_app.tsx
+++ blogr-nextjs-prisma/pages/_app.tsx
@@ -2,6 +2,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { wrapper } from '../store';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+(toast as any).configure();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
