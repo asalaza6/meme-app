5124ade48b33b5627e5b8e4ef07caa30e55aecf4
Alex Salazar <55558620+asalaza6@users.noreply.github.com>
2022-07-18 02:07:56 -0700

    secure image upload

--- blogr-nextjs-prisma/pages/api/upload.ts
+++ blogr-nextjs-prisma/pages/api/upload.ts
@@ -5,6 +5,7 @@ import { jwtGenerator } from '../../utils/jwtGenerator';
 import bcrypt from 'bcryptjs';
 import { v2 as cloudinary } from 'cloudinary'
 import withProtect from '../../middleware/withUser';
+var FormData = require('form-data');
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
@@ -21,17 +22,35 @@ const handler = async function handle(req, res) {
         return res.status(500).send( `Invalid folder ${folder}`);
     }
     const timestamp = Math.round((new Date).getTime()/1000);
+    const signature = await cloudinary.utils.api_sign_request(
+      {
+        timestamp,
+        folder
+      },
+      process.env.API_SECRET
+    );
+    const form = new FormData();
+    form.append('file', content);
+    form.append('folder', folder);
     try{
-        const response = await cloudinary.uploader.upload(
-            content,
+        const cloudinaryRes = await fetch(
+            `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/image/upload?api_key=${process.env.API_KEY}&timestamp=${timestamp}&signature=${signature}`,
             {
-                folder,
-                timestamp,
-            },
-            function(error, result) {
-                console.log('callback', result, error)
-            },
-        );
+              method: 'POST',
+              body: form,
+            }
+          )
+          const response = await cloudinaryRes.json();
+        // const response = await cloudinary.uploader.upload(
+        //     content,
+        //     {
+        //         folder,
+        //         timestamp,
+        //     },
+        //     function(error, result) {
+        //         console.log('callback', result, error)
+        //     },
+        // );
         const {
             secure_url,
             url,
